7ad2adbcb2a312a48dea3542d0fcebfe21382a3f
7Brandyn <brandyn.evans7@gmail.com>
2022-12-04 19:04:58 -0800

    bump

--- playground/blackjack/script.js
+++ playground/blackjack/script.js
@@ -1,13 +1,20 @@
 let firstCard = 10
 let secondCard = 4
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el");
 let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.querySelector("#cards-el");
+let newDeckText = "Drawing a new card from the deck!";
 
 function startGame() {
+    renderGame();
+}
+
+function renderGame() {
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -18,5 +25,13 @@ function startGame() {
         isAlive = false
     }
     messageEl.textContent = message;
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
     sumEl.textContent = "Sum: " + sum;
+}
+
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+        let card = 7
+        sum += card
+        startGame()
 }
